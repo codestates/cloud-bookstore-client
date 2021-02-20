50b15bded4b0da44472c67e115b638d9f6924385
danielsychoo <choointern@gmail.com>
2021-02-20 18:44:12 +0900

    [0-2] Update App.tsx like a boiler plate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">for test first page</div>;
+  return <div className="App">Hello FinalProject World! :)</div>;
 }
 
 export default App;
