eb5f2b5170a1d21da55093c5871b75fead2255fb
danielsychoo <choointern@gmail.com>
2021-02-22 13:25:31 +0900

    [1-1] Update position of Landing page
    landing page의 header, content, footer의 각 부분과
    content내의 소그룹들을 디자인 시안에 맞추어 크기지정
    및 배치 완료

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,8 +4,19 @@ import '../css/Landing.css';
 function Landing() {
   return (
     <>
-      <div className="landingHeader">header</div>
-      <div className="landingContent">content</div>
+      <div className="landingHeader">
+        <div className="landingMenuBar">menu</div>
+        <div className="landingHeaderContent">header-content</div>
+      </div>
+      <div className="landingContent">
+        <div className="landingBestNovel">독자들이 선택한 최고의 작품들</div>
+        <div className="landingWriteNovel">
+          상상을 글로 옮겨 간편하게 작가되기
+        </div>
+        <div className="landingWatchFunction">
+          한 눈에 살펴보는 구름책방의 기능들
+        </div>
+      </div>
       <div className="footer">footer</div>
     </>
   );
