cb1d53a112577b4ba6b1ca984f715b198dba80d8
danielsychoo <choointern@gmail.com>
2021-02-22 19:54:18 +0900

    [1-3] Update write novel wrapper

--- src/pages/Landingpage.tsx
+++ src/pages/Landingpage.tsx
@@ -105,7 +105,23 @@ function Landingpage() {
           </div>
         </div>
         <div className="landingWriteNovel">
-          상상을 글로 옮겨 간편하게 작가되기
+          <div className="landingWriteNovelText">
+            <div className="LWNTbold">
+              <p>상상을 글로 옮겨</p>
+              <p>간편하게 작가되기</p>
+            </div>
+            <div className="LWNTlight">
+              <p>구름책방에서는 챌린지를 거치지 않아도</p>
+              <p>누구나 작가가 될 수 있습니다.</p>
+            </div>
+          </div>
+          <div className="landingWriteImg">
+            <div className="landingWriteLure">
+              <p>작품의 첫 페이지를 작성해</p>
+              <p>작가로서의 여정을 시작해보세요.</p>
+              <span className="landingWriteBtn">글쓰러 가기</span>
+            </div>
+          </div>
         </div>
         <div className="landingWatchFunction">
           한 눈에 살펴보는 구름책방의 기능들
