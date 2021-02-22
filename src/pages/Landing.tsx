a5f4aeca477f385dc01cf01faff7900cd6e42fc4
danielsychoo <choointern@gmail.com>
2021-02-22 12:06:23 +0900

    [1-1] Create pngs and Landingpage
    디자인에 사용될 png들을 asset dir에 넣어두고
    Landingpage를 작업하기 위한 component와 css파일을
    생성

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/Landing.css';
+
+function Landing() {
+  return (
+    <>
+      <div className="landingHeader">header</div>
+      <div className="landingContent">content</div>
+      <div className="footer">footer</div>
+    </>
+  );
+}
+
+export default Landing;
