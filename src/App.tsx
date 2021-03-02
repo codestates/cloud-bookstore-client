e5557e06d26ef421eebc2e74563adf9b425e12bb
Sanghyuk Choi <nugu1223@gmail.com>
2021-03-02 14:26:26 +0900

    Update LoginModal css
    loginmodal 소셜로그인 기능 및 반응형 대응

--- src/App.tsx
+++ src/App.tsx
@@ -87,7 +87,7 @@ const App: React.FC = () => {
           path="/main"
           render={() => (
             <Mainpage
-              isLogin={!isLogin}
+              isLogin={isLogin}
               toggleLogin={toggleLogin}
               nickname={nickname}
               handleNickname={handleNickname}
