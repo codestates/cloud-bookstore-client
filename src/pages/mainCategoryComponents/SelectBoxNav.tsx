53e46e27ec25d9a9d9a38db8d6b83778ebac651e
danielsychoo <choointern@gmail.com>
2021-03-04 15:32:43 +0900

    Fix data name in Categories

--- src/pages/mainCategoryComponents/SelectBoxNav.tsx
+++ src/pages/mainCategoryComponents/SelectBoxNav.tsx
@@ -48,8 +48,8 @@ const options: OptionType[] = [
   { value: 'favorite', label: '관심순' },
 ];
 
-interface FakeCategorizedDataProps {
-  CategorizedData: {
+interface CategorizedDataProps {
+  categorizedData: {
     data: {
       id: number;
       title: string;
@@ -66,12 +66,12 @@ interface FakeCategorizedDataProps {
   };
 }
 
-class SelectBoxNav extends Component<FakeCategorizedDataProps, State> {
-  constructor(props: FakeCategorizedDataProps) {
+class SelectBoxNav extends Component<CategorizedDataProps, State> {
+  constructor(props: CategorizedDataProps) {
     super(props);
     this.state = {
       selectedOption: options[0],
-      sortedData: this.props.CategorizedData.data,
+      sortedData: this.props.categorizedData.data,
     };
     this.handleChange = this.handleChange.bind(this);
     this.dataSortHandler = this.dataSortHandler.bind(this);
@@ -117,7 +117,7 @@ class SelectBoxNav extends Component<FakeCategorizedDataProps, State> {
 
   // ? update 기준 sort
   handleSortWithUpdated = (): void => {
-    const temp = this.props.CategorizedData.data;
+    const temp = this.props.categorizedData.data;
     temp.sort((a: data, b: data) => {
       if (a.updatedAt < b.updatedAt) return 1;
       if (a.updatedAt > b.updatedAt) return -1;
@@ -131,7 +131,7 @@ class SelectBoxNav extends Component<FakeCategorizedDataProps, State> {
   };
   // ? title 기준 sort
   handleSortWithTitle = (): void => {
-    const temp = this.props.CategorizedData.data;
+    const temp = this.props.categorizedData.data;
     temp.sort((a: data, b: data) => {
       if (a.title < b.title) return -1;
       if (a.title > b.title) return 1;
@@ -145,7 +145,7 @@ class SelectBoxNav extends Component<FakeCategorizedDataProps, State> {
   };
   // ? 누적 cloud 기준 sort
   handleSortWithCloud = (): void => {
-    const temp = this.props.CategorizedData.data;
+    const temp = this.props.categorizedData.data;
     temp.sort((a: data, b: data) => {
       if (a.cloud < b.cloud) return 1;
       if (a.cloud > b.cloud) return -1;
@@ -159,7 +159,7 @@ class SelectBoxNav extends Component<FakeCategorizedDataProps, State> {
   };
   // ? 관심 기준 sort
   handleSortWithFavoriteData = (): void => {
-    const temp = this.props.CategorizedData.data;
+    const temp = this.props.categorizedData.data;
     temp.sort((a: data, b: data) => {
       if (a.userLike < b.userLike) return 1;
       if (a.userLike > b.userLike) return -1;
--- src/pages/mainCategoryComponents/martialArtsCategoryComponents/MartialArtsCategory.tsx
+++ src/pages/mainCategoryComponents/martialArtsCategoryComponents/MartialArtsCategory.tsx
@@ -1,48 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './MartialArtsCategory.css';
+import axios from 'axios';
 
 import SelectBoxNav from '../SelectBoxNav';
 
-const fakeCategorizedData = {
-  data: [
-    {
-      id: 1,
-      title: '이곳까지 올라와 보시게나',
-      author: '유단잔가',
-      category: 1,
-      description:
-        '새침하게 흐린 품이 눈이 올 듯하더니 눈은 아니 오고 얼다가 만 비가 추적추적 내리는 날이었다.\r\n\n이날이야말로 동소문 안에서 인력거꾼 노릇을 하는 김첨지에게는 오래간만에도 닥친 운수 좋은 날...',
-      cloud: 0,
-      userLike: 1,
-      complete: false,
-      thumbnail:
-        'https://user-images.githubusercontent.com/72306693/108985620-99c68280-76d4-11eb-9305-50ef35e77c93.png',
-      createdAt: '2021-02-23T21:20:11.925Z',
-      updatedAt: '2021-02-24T00:17:06.955Z',
-    },
-    {
-      id: 3,
-      title: '무림별곡',
-      author: '드루와',
-      category: 1,
-      description:
-        '새침하게 흐린 품이 눈이 올 듯하더니 눈은 아니 오고 얼다가 만 비가 추적추적 내리는 날이었다.\r\n\n이날이야말로 동소문 안에서 인력거꾼 노릇을 하는 김첨지에게는 오래간만에도 닥친 운수 좋은 날...',
-      cloud: 1,
-      userLike: 0,
-      complete: true,
-      thumbnail:
-        'https://user-images.githubusercontent.com/72306693/108985620-99c68280-76d4-11eb-9305-50ef35e77c93.png',
-      createdAt: '2021-02-24T23:41:28.712Z',
-      updatedAt: '2021-03-03T00:17:19.770Z',
-    },
-  ],
-};
+interface FantasyCategoryData {
+  data: {
+    id: number;
+    title: string;
+    author: string;
+    category: number;
+    description: string;
+    cloud: number;
+    userLike: number;
+    complete: boolean;
+    thumbnail: string;
+    createdAt: string;
+    updatedAt: string;
+  }[];
+}
 
 const MartialArtsCategory: React.FC = () => {
+  const [
+    martialArtsNovelData,
+    setMartialArtsNovelData,
+  ] = useState<FantasyCategoryData>({
+    data: [
+      {
+        id: 1,
+        title: '잊지마 4시1분',
+        author: '투투니깐 200원 줘',
+        category: 3,
+        description:
+          '새침하게 흐린 품이 눈이 올 듯하더니 눈은 아니 오고 얼다가 만 비가 추적추적 내리는 날이었다.\r\n\n이날이야말로 동소문 안에서 인력거꾼 노릇을 하는 김첨지에게는 오래간만에도 닥친 운수 좋은 날...',
+        cloud: 0,
+        userLike: 0,
+        complete: false,
+        thumbnail:
+          'https://user-images.githubusercontent.com/72306693/108985620-99c68280-76d4-11eb-9305-50ef35e77c93.png',
+        createdAt: '2021-02-24T21:20:11.925Z',
+        updatedAt: '2021-02-25T00:17:06.955Z',
+      },
+    ],
+  });
+
+  useEffect(() => {
+    axios.get('https://server.cloud-bookstore.com/category/2').then((res) => {
+      setMartialArtsNovelData(res.data);
+    });
+  }, []);
+
   return (
     <div className="wholeFantasyCategoryWrapper">
       <div className="fantasyCategoryInnerWrapper">
-        <SelectBoxNav fakeCategorizedData={fakeCategorizedData} />
+        <SelectBoxNav categorizedData={martialArtsNovelData} />
       </div>
     </div>
   );
