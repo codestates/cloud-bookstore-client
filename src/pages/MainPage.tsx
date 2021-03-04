65e1c5b6358e78a10f5eef0450964b16ae987a84
danielsychoo <choointern@gmail.com>
2021-03-04 20:02:41 +0900

    Update Category data with axios

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   RouteComponentProps,
   withRouter,
   Route,
   Switch,
 } from 'react-router-dom';
+import axios from 'axios';
 import '../css/MainPage.css';
 import '../css/CategoryNav.css';
 import { MdSearch } from 'react-icons/md';
@@ -91,6 +92,89 @@ const MainPage: React.FC<mainPageProps> = (props: mainPageProps) => {
   const handleMyOn = (): void => {
     setIsCategoryOn('/main/mypage/recentNovelList');
   };
+
+  // ! Fantasy novel Data - axios get
+  const [fantasyNovelData, setFantasyNovelData] = useState({
+    data: [
+      {
+        id: 1,
+        title: '잊지마 4시1분',
+        author: '투투니깐 200원 줘',
+        category: 3,
+        description:
+          '새침하게 흐린 품이 눈이 올 듯하더니 눈은 아니 오고 얼다가 만 비가 추적추적 내리는 날이었다.\r\n\n이날이야말로 동소문 안에서 인력거꾼 노릇을 하는 김첨지에게는 오래간만에도 닥친 운수 좋은 날...',
+        cloud: 0,
+        userLike: 0,
+        complete: false,
+        thumbnail:
+          'https://user-images.githubusercontent.com/72306693/108985620-99c68280-76d4-11eb-9305-50ef35e77c93.png',
+        createdAt: '2021-02-24T21:20:11.925Z',
+        updatedAt: '2021-02-25T00:17:06.955Z',
+      },
+    ],
+  });
+  const handleAxiosFantasy = () => {
+    axios.get('https://server.cloud-bookstore.com/category/1').then((res) => {
+      setFantasyNovelData(res.data);
+    });
+  }
+  // ! MartialArts novel Data - axios get
+  const [martialArtsNovelData, setmartialArtsNovelData] = useState({
+    data: [
+      {
+        id: 1,
+        title: '잊지마 4시1분',
+        author: '투투니깐 200원 줘',
+        category: 3,
+        description:
+          '새침하게 흐린 품이 눈이 올 듯하더니 눈은 아니 오고 얼다가 만 비가 추적추적 내리는 날이었다.\r\n\n이날이야말로 동소문 안에서 인력거꾼 노릇을 하는 김첨지에게는 오래간만에도 닥친 운수 좋은 날...',
+        cloud: 0,
+        userLike: 0,
+        complete: false,
+        thumbnail:
+          'https://user-images.githubusercontent.com/72306693/108985620-99c68280-76d4-11eb-9305-50ef35e77c93.png',
+        createdAt: '2021-02-24T21:20:11.925Z',
+        updatedAt: '2021-02-25T00:17:06.955Z',
+      },
+    ],
+  });
+  const handleAxiosMartialArts = () => {
+    axios.get('https://server.cloud-bookstore.com/category/2').then((res) => {
+      setmartialArtsNovelData(res.data);
+    });
+  }
+  // ! Romance novel Data - axios get
+  const [romanceNovelData, setRomanceNovelData] = useState({
+    data: [
+      {
+        id: 1,
+        title: '잊지마 4시1분',
+        author: '투투니깐 200원 줘',
+        category: 3,
+        description:
+          '새침하게 흐린 품이 눈이 올 듯하더니 눈은 아니 오고 얼다가 만 비가 추적추적 내리는 날이었다.\r\n\n이날이야말로 동소문 안에서 인력거꾼 노릇을 하는 김첨지에게는 오래간만에도 닥친 운수 좋은 날...',
+        cloud: 0,
+        userLike: 0,
+        complete: false,
+        thumbnail:
+          'https://user-images.githubusercontent.com/72306693/108985620-99c68280-76d4-11eb-9305-50ef35e77c93.png',
+        createdAt: '2021-02-24T21:20:11.925Z',
+        updatedAt: '2021-02-25T00:17:06.955Z',
+      },
+    ],
+  });
+  const handleAxiosRomance = () => {
+    axios.get('https://server.cloud-bookstore.com/category/3').then((res) => {
+      setRomanceNovelData(res.data);
+    });
+  }
+  // ! handleAxios 함수들 미리 실행시켜두기
+  useEffect(() => {
+    handleAxiosFantasy();
+    handleAxiosMartialArts();
+    handleAxiosRomance();
+  }, []);
+
   return (
     <div>
       <div className="wholeMainPageWrapper">
@@ -207,12 +291,12 @@ const MainPage: React.FC<mainPageProps> = (props: mainPageProps) => {
               />
             )}
           />
-          <Route path="/main/fantasy" render={() => <FantasyCategory />} />
+          <Route path="/main/fantasy" render={() => <FantasyCategory fantasyNovelData={fantasyNovelData} />} />
           <Route
             path="/main/martialArts"
-            render={() => <MartialArtsCategory />}
+            render={() => <MartialArtsCategory martialArtsNovelData={martialArtsNovelData} />}
           />
-          <Route path="/main/romance" render={() => <RomanceCategory />} />
+          <Route path="/main/romance" render={() => <RomanceCategory romanceNovelData={romanceNovelData} />} />
           <Route path="/main/mypage" render={() => <Mypage />} />
           <Route
             path="/main/mypage/recentNovelList"
