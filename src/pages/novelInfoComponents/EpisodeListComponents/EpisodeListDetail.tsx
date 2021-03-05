c6dbfe495a2f326a25e071a20de9c2d4dd8a1e01
paigekim29 <paigekim29@gmail.com>
2021-03-05 12:06:58 +0900

    [4-1]Update css
    novel list css 90% 완료

---
+++ src/pages/novelInfoComponents/EpisodeListComponents/EpisodeListDetail.tsx
@@ -0,0 +1,147 @@
+import React from 'react';
+
+const EpisodeListDetail: React.FC = () => {
+  return (
+    <>
+      <div className="novelList">
+        <div className="thumbnail">
+          <div className="novelListCompleteObject">완결</div>
+        </div>
+        <div className="homeNovelListContentWrapper">
+          <div className="countCloud">
+            <div className="countCloudText">누적구름</div>
+            <div className="countCloudImg" />
+          </div>
+          <div className="novelListSubjectWrapper">
+            <div className="HomeNovelListSubject">title</div>
+          </div>
+          <div className="homeNovelListAuthorFavWrapper">
+            <div className="novelListAuthor">updatedAt</div>
+          </div>
+        </div>
+      </div>
+      <div className="novelList">
+        <div className="thumbnail">
+          <div className="novelListCompleteObject">완결</div>
+        </div>
+        <div className="homeNovelListContentWrapper">
+          <div className="countCloud">
+            <div className="countCloudText">누적구름</div>
+            <div className="countCloudImg" />
+            <div className="novelListNewObject">NEW</div>
+          </div>
+          <div className="novelListSubjectWrapper">
+            <div className="HomeNovelListSubject">title</div>
+          </div>
+          <div className="homeNovelListAuthorFavWrapper">
+            <div className="novelListAuthor">updatedAt</div>
+          </div>
+        </div>
+      </div>
+      <div className="novelList">
+        <div className="thumbnail">
+          <div className="novelListCompleteObject">완결</div>
+        </div>
+        <div className="homeNovelListContentWrapper">
+          <div className="countCloud">
+            <div className="countCloudText">누적구름</div>
+            <div className="countCloudImg" />
+          </div>
+          <div className="novelListSubjectWrapper">
+            <div className="HomeNovelListSubject">title</div>
+          </div>
+          <div className="homeNovelListAuthorFavWrapper">
+            <div className="novelListAuthor">updatedAt</div>
+          </div>
+        </div>
+      </div>
+      <div className="novelList">
+        <div className="thumbnail">
+          <div className="novelListCompleteObject">완결</div>
+        </div>
+        <div className="homeNovelListContentWrapper">
+          <div className="countCloud">
+            <div className="countCloudText">누적구름</div>
+            <div className="countCloudImg" />
+          </div>
+          <div className="novelListSubjectWrapper">
+            <div className="HomeNovelListSubject">title</div>
+          </div>
+          <div className="homeNovelListAuthorFavWrapper">
+            <div className="novelListAuthor">updatedAt</div>
+          </div>
+        </div>
+      </div>
+      <div className="novelList">
+        <div className="thumbnail">
+          <div className="novelListCompleteObject">완결</div>
+        </div>
+        <div className="homeNovelListContentWrapper">
+          <div className="countCloud">
+            <div className="countCloudText">누적구름</div>
+            <div className="countCloudImg" />
+          </div>
+          <div className="novelListSubjectWrapper">
+            <div className="HomeNovelListSubject">title</div>
+          </div>
+          <div className="homeNovelListAuthorFavWrapper">
+            <div className="novelListAuthor">updatedAt</div>
+          </div>
+        </div>
+      </div>
+      <div className="novelList">
+        <div className="thumbnail">
+          <div className="novelListCompleteObject">완결</div>
+        </div>
+        <div className="homeNovelListContentWrapper">
+          <div className="countCloud">
+            <div className="countCloudText">누적구름</div>
+            <div className="countCloudImg" />
+          </div>
+          <div className="novelListSubjectWrapper">
+            <div className="HomeNovelListSubject">title</div>
+          </div>
+          <div className="homeNovelListAuthorFavWrapper">
+            <div className="novelListAuthor">updatedAt</div>
+          </div>
+        </div>
+      </div>
+      <div className="novelList">
+        <div className="thumbnail">
+          <div className="novelListCompleteObject">완결</div>
+        </div>
+        <div className="homeNovelListContentWrapper">
+          <div className="countCloud">
+            <div className="countCloudText">누적구름</div>
+            <div className="countCloudImg" />
+          </div>
+          <div className="novelListSubjectWrapper">
+            <div className="HomeNovelListSubject">title</div>
+          </div>
+          <div className="homeNovelListAuthorFavWrapper">
+            <div className="novelListAuthor">updatedAt</div>
+          </div>
+        </div>
+      </div>
+      <div className="novelList">
+        <div className="thumbnail">
+          <div className="novelListCompleteObject">완결</div>
+        </div>
+        <div className="homeNovelListContentWrapper">
+          <div className="countCloud">
+            <div className="countCloudText">누적구름</div>
+            <div className="countCloudImg" />
+          </div>
+          <div className="novelListSubjectWrapper">
+            <div className="HomeNovelListSubject">title</div>
+          </div>
+          <div className="homeNovelListAuthorFavWrapper">
+            <div className="novelListAuthor">updatedAt</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EpisodeListDetail;
