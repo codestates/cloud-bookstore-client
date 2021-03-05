aa2cb2f10a087db955c10a63dadce7aa934bcf7a
danielsychoo <choointern@gmail.com>
2021-03-05 16:35:33 +0900

    Delete get id func

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -103,12 +103,6 @@ const MainPage: React.FC<mainPageProps> = (props: mainPageProps) => {
     setIsCategoryOn('/main/mypage/recentNovelList');
   };
 
-  // ? id 가져오기이이이이이이ㅣㅇ 제발가져와라
-  // const [clickedNovelId, setClickedNovelId] = useState<number>(0);
-  // const handleClickedNovelId = (parameter: number) => {
-  //   setClickedNovelId(parameter);
-  // };
-
   const [clickedNovelData, setClickedNovelData] = useState({
     data: {
       id: 1,
@@ -440,7 +434,6 @@ const MainPage: React.FC<mainPageProps> = (props: mainPageProps) => {
                 handleFantasyOn={handleFantasyOn}
                 handleMartialArtOn={handleMartialArtOn}
                 handleRomanceOn={handleRomanceOn}
-                // handleClickedNovelId={handleClickedNovelId}
                 handleAxiosClickedNovelData={handleAxiosClickedNovelData}
               />
             )}
