94a72555b2ceb3763d9b47e41304595d77981f6c
danielsychoo <choointern@gmail.com>
2021-03-08 12:09:37 +0900

    Create edit & delete Comment

--- src/pages/novelInfoComponents/commentComponents/CommentsList.tsx
+++ src/pages/novelInfoComponents/commentComponents/CommentsList.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CommentsList.css';
+import axios from 'axios';
 
 interface CommentsDataProps {
+  handleAxiosClickedNovelData: (parameter: number) => void;
   data: {
     id: number;
     nickname: string;
     comment: string;
     novelId: number;
+    createdAt: string;
+    updatedAt: string;
+  };
+  novelData: {
+    id: number;
+    title: string;
+    author: string;
+    category: number;
+    description: string;
+    cloud: number;
+    userLike: number;
+    episodeCount: number;
+    complete: boolean;
+    thumbnail: string;
     createdAt: string;
     updatedAt: string;
   };
@@ -17,6 +33,44 @@ const CommentsList: React.FC<CommentsDataProps> = (
 ) => {
   const refinedUpdatedAt: string = props.data.updatedAt.slice(0, 10);
 
+  const [editComment, setEditComment] = useState<boolean>(false);
+  const handleEditCommentOn = () => {
+    setEditComment(!editComment);
+  };
+
+  const [editCommentText, setEditCommentText] = useState<string>(
+    props.data.comment,
+  );
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
+    setEditCommentText(e.target.value);
+  };
+
+  const handleEditComment = () => {
+    axios
+      .patch(`https://server.cloud-bookstore.com/novel/comment/edit`, {
+        data: {
+          novelId: props.novelData.id,
+          comment: editCommentText,
+          commentId: props.data.id,
+        },
+      })
+      .then(() => props.handleAxiosClickedNovelData(props.novelData.id));
+  };
+
+  const handleDeleteComment = () => {
+    axios
+      .delete(
+        `https://server.cloud-bookstore.com/novel/comment/delete/${props.novelData.id}/${props.data.id}`,
+        {
+          data: {
+            novelId: props.novelData.id,
+            commentId: props.data.id,
+          },
+        },
+      )
+      .then(() => props.handleAxiosClickedNovelData(props.novelData.id));
+  };
+
   return (
     <div className="wholeCommentsListWrapper">
       <div className="commentsListUserInfo">
@@ -25,11 +79,51 @@ const CommentsList: React.FC<CommentsDataProps> = (
           <div className="commentsListDate">{refinedUpdatedAt}</div>
         </div>
         <div className="commentsListEditDelWrapper">
-          <div className="commentsListEditBtn">수정</div>
-          <div className="commentsListDeleteBtn">삭제</div>
+          {editComment ? (
+            <div
+              role="presentation"
+              className="commentsListEditBtn"
+              onClick={() => handleEditComment()}
+            >
+              확인
+            </div>
+          ) : (
+            <div
+              role="presentation"
+              className="commentsListEditBtn"
+              onClick={() => handleEditCommentOn()}
+            >
+              수정
+            </div>
+          )}
+          {editComment ? (
+            <div
+              role="presentation"
+              className="commentsListDeleteBtn"
+              onClick={() => handleEditCommentOn()}
+            >
+              취소
+            </div>
+          ) : (
+            <div
+              role="presentation"
+              className="commentsListDeleteBtn"
+              onClick={() => handleDeleteComment()}
+            >
+              삭제
+            </div>
+          )}
         </div>
       </div>
-      <div className="commentsListMainText">{props.data.comment}</div>
+      {editComment ? (
+        <textarea
+          className="commentsListEditText"
+          onChange={handleChange}
+          maxLength={500}
+        ></textarea>
+      ) : (
+        <div className="commentsListMainText">{props.data.comment}</div>
+      )}
     </div>
   );
 };
