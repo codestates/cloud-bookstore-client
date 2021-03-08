2800688a856b5cb4b84a2e09a09084929a6e2122
danielsychoo <choointern@gmail.com>
2021-03-08 15:30:59 +0900

    Update landingpage bestnovel with axios-get

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import '../css/LandingPage.css';
+import axios from 'axios';
 
 import Cover from './landingPageComponent/Cover';
 import BestNovel from './landingPageComponent/BestNovel';
@@ -12,6 +13,87 @@ import Footer from './Footer';
 
 const LandingPage: React.FC = () => {
   AOS.init();
+
+  const [novelData, setNovelData] = useState({
+    ranking: [
+      {
+        id: 20,
+        title: '눈떠보니 아르미르신전에 내가 서있었다',
+        author: '풍렬',
+        category: 1,
+        description:
+          '새침하게 흐린 품이 눈이 올 듯하더니 눈은 아니 오고 얼다가 만 비가 추적추적 내리는 날이었다.\r\n\n이날이야말로 동소문 안에서 인력거꾼 노릇을 하는 김첨지에게는 오래간만에도 닥친 운수 좋은 날...',
+        cloud: 222,
+        userLike: 143,
+        episodeCount: 0,
+        complete: true,
+        thumbnail:
+          'https://user-images.githubusercontent.com/72306693/108985620-99c68280-76d4-11eb-9305-50ef35e77c93.png',
+        createdAt: '2021-02-25T15:55:19.608Z',
+        updatedAt: '2021-03-05:20:08.168Z',
+      },
+    ],
+    fantasy: [
+      {
+        id: 20,
+        title: '눈떠보니 아르미르신전에 내가 서있었다',
+        author: '풍렬',
+        category: 1,
+        description:
+          '새침하게 흐린 품이 눈이 올 듯하더니 눈은 아니 오고 얼다가 만 비가 추적추적 내리는 날이었다.\r\n\n이날이야말로 동소문 안에서 인력거꾼 노릇을 하는 김첨지에게는 오래간만에도 닥친 운수 좋은 날...',
+        cloud: 222,
+        userLike: 143,
+        episodeCount: 0,
+        complete: false,
+        thumbnail:
+          'https://user-images.githubusercontent.com/72306693/108985620-99c68280-76d4-11eb-9305-50ef35e77c93.png',
+        createdAt: '2021-02-25T15:55:19.608Z',
+        updatedAt: '2021-02-28T11:20:08.168Z',
+      },
+    ],
+    martialArts: [
+      {
+        id: 15,
+        title: '복수의 전쟁',
+        author: '풍렬',
+        category: 2,
+        description:
+          '새침하게 흐린 품이 눈이 올 듯하더니 눈은 아니 오고 얼다가 만 비가 추적추적 내리는 날이었다.\r\n\n이날이야말로 동소문 안에서 인력거꾼 노릇을 하는 김첨지에게는 오래간만에도 닥친 운수 좋은 날...',
+        cloud: 90,
+        userLike: 50,
+        episodeCount: 0,
+        complete: false,
+        thumbnail:
+          'https://user-images.githubusercontent.com/72306693/108985620-99c68280-76d4-11eb-9305-50ef35e77c93.png',
+        createdAt: '2021-02-25T15:54:40.290Z',
+        updatedAt: '2021-02-28T11:36:09.412Z',
+      },
+    ],
+    romance: [
+      {
+        id: 9,
+        title: '우리 집 옆에는 살인범이 산다?',
+        author: 'paige',
+        category: 3,
+        description:
+          '새침하게 흐린 품이 눈이 올 듯하더니 눈은 아니 오고 얼다가 만 비가 추적추적 내리는 날이었다.\r\n\n이날이야말로 동소문 안에서 인력거꾼 노릇을 하는 김첨지에게는 오래간만에도 닥친 운수 좋은 날...',
+        cloud: 100,
+        userLike: 50,
+        episodeCount: 0,
+        complete: false,
+        thumbnail:
+          'https://user-images.githubusercontent.com/72306693/108985620-99c68280-76d4-11eb-9305-50ef35e77c93.png',
+        createdAt: '2021-02-25T15:47:09.599Z',
+        updatedAt: '2021-02-28T11:36:01.641Z',
+      },
+    ],
+  });
+
+  useEffect(() => {
+    axios.get('https://server.cloud-bookstore.com/novels').then((res) => {
+      setNovelData(res.data);
+    });
+  }, []);
   return (
     <div className="landingBody">
       <div className="wholeLandingPageWrapper">
@@ -21,12 +103,12 @@ const LandingPage: React.FC = () => {
               <div className="logoBtn"></div>
             </a>
             <a href="http://cloud-bookstore.com/main/home">
-              <div className="goToMainBtn">소설읽기</div>
+              <div className="goToMainBtn">시작하기</div>
             </a>
           </div>
         </div>
         <Cover />
-        <BestNovel />
+        <BestNovel novelData={novelData} />
         <WriteLure />
         <IntroFunction />
         <div id="verticalLine1" className="verticalLine"></div>
--- src/pages/landingPageComponent/BestNovel.tsx
+++ src/pages/landingPageComponent/BestNovel.tsx
@@ -3,7 +3,68 @@ import './BestNovel.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-const BestNovel: React.FC = () => {
+interface BestNovelProps {
+  novelData: {
+    ranking: {
+      id: number;
+      title: string;
+      author: string;
+      category: number;
+      description: string;
+      cloud: number;
+      userLike: number;
+      episodeCount: number;
+      complete: boolean;
+      thumbnail: string;
+      createdAt: string;
+      updatedAt: string;
+    }[];
+    fantasy: {
+      id: number;
+      title: string;
+      author: string;
+      category: number;
+      description: string;
+      cloud: number;
+      userLike: number;
+      episodeCount: number;
+      complete: boolean;
+      thumbnail: string;
+      createdAt: string;
+      updatedAt: string;
+    }[];
+    martialArts: {
+      id: number;
+      title: string;
+      author: string;
+      category: number;
+      description: string;
+      cloud: number;
+      userLike: number;
+      episodeCount: number;
+      complete: boolean;
+      thumbnail: string;
+      createdAt: string;
+      updatedAt: string;
+    }[];
+    romance: {
+      id: number;
+      title: string;
+      author: string;
+      category: number;
+      description: string;
+      cloud: number;
+      userLike: number;
+      episodeCount: number;
+      complete: boolean;
+      thumbnail: string;
+      createdAt: string;
+      updatedAt: string;
+    }[];
+  };
+}
+
+const BestNovel: React.FC<BestNovelProps> = (props: BestNovelProps) => {
   AOS.init();
   return (
     <div className="wholeBestNovelWrapper">
@@ -19,14 +80,9 @@ const BestNovel: React.FC = () => {
         data-aos-anchor-placement="top-bottom"
       >
         <div id="bestNovelThumb1" className="bestNovelThumb"></div>
-        <div className="bestNovelTitle">완벽한 부부는 없다</div>
+        <div className="bestNovelTitle">{props.novelData.fantasy[0].title}</div>
         <div className="bestNovelDescription">
-          결혼기념일 당일, 남편이 정략결혼의 끝을 알렸다. “이혼해, 우리.”
-          오랫동안 건우를 짝사랑해 온 연화는 그의 회사로 찾아가 이혼을 거절하고
-          돌아가던 중 사고를 당하고 만다. “누구세요.” 이혼을 피하고 싶던 연화는
-          기억을 잃은 척 연기하는데…… 갑자기 남편이 변했다? “같이 씻어.” “같이
-          씻어요?” “그래왔으니까, 매번.” 우리가 언제? 이 남자 도대체 무슨
-          꿍꿍이야! 과연, 불완전한 우리가 완벽한 부부가 될 수 있을까?
+          {props.novelData.fantasy[0].description}
         </div>
       </div>
       <div
@@ -37,14 +93,11 @@ const BestNovel: React.FC = () => {
         data-aos-offset="300"
       >
         <div id="bestNovelThumb2" className="bestNovelThumb"></div>
-        <div className="bestNovelTitle">MANSLAYR [맨슬레이어]</div>
+        <div className="bestNovelTitle">
+          {props.novelData.martialArts[0].title}
+        </div>
         <div className="bestNovelDescription">
-          여동생 한서아를 위해 살인죄를 쓰게 된 이지훈은 결국 감옥에서
-          사형당한다. 하지만 그가 죽은 후 마지막 기회를 준다며 GM이 나타나게
-          되는데…. &#34;내가 만든 게임에서 이기면 소원 3가지를 이루어 줄게, 가령
-          다시 살아난다든지 여동생이 괴롭힘 받지 않는 그런 소원 말이야&#34;
-          &#34;그래, 어차피 이기면 되는 거잖아? 내가 전부 엎어버리겠어!&#34;
-          매주 일요일 오후 6시에 연재 됩니다.
+          {props.novelData.martialArts[0].description}
         </div>
       </div>
       <div
@@ -55,16 +108,9 @@ const BestNovel: React.FC = () => {
         data-aos-offset="500"
       >
         <div id="bestNovelThumb3" className="bestNovelThumb"></div>
-        <div className="bestNovelTitle">H - Dragon</div>
+        <div className="bestNovelTitle">{props.novelData.romance[0].title}</div>
         <div className="bestNovelDescription">
-          기억도 잘 나지 않는 중학생 시절, 엄마와 사별하고 강해지려고 했던
-          각오가 무색하게 아버지가 몸져 누우셨다. 고등학생이 짊어지기 버거운
-          삶의 무게에 병원 옥상에서 투신자살을 택하는데...... 그런데 어라?
-          정신차려보니 아무것도 안보이는 캄캄한 곳─용의 알이라고?! 감옥에서 단
-          둘이 은밀한 시간을 보내거나, 알몸으로 거리 배회, 주인님과 노예
-          플레이...... 신에게 부여받은 마지막 기회. 아빠를 구할 수 있다면
-          망설이지 않겠어. 애들은 저리가라. 검열 없는
-          단편막장코믹액션배틀판타지! 에이치 드래곤, 지금 시작합니다.
+          {props.novelData.romance[0].description}
         </div>
       </div>
     </div>
