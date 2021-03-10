eb334ab1133d0a1b54ad866fae06fca0c2e291b9
Sanghyuk Choi <nugu1223@gmail.com>
2021-03-10 17:18:40 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -459,7 +459,7 @@ const App: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
               setSpecificEpisodeData={setSpecificEpisodeData}
               clickedNovelData={clickedNovelData}
               handleAxiosClickedNovelData={handleAxiosClickedNovelData}
-              isLogin={!isLogin}
+              isLogin={isLogin}
               toggleLogin={toggleLogin}
               nickname={nickname}
               handleNickname={handleNickname}
